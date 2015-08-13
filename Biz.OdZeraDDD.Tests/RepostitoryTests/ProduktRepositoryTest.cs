1b29307c3edfc2bf1b4393a0276e3ec0a754e486
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2015-08-13 17:44:37 +0200

    Implementacja testu repozytorium produktu

--- Biz.OdZeraDDD.Tests/RepostitoryTests/ProduktRepositoryTest.cs
+++ Biz.OdZeraDDD.Tests/RepostitoryTests/ProduktRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using Biz.OdZeraDDD.Model.DomainModel;
 using Biz.OdZeraDDD.Model.Persistence.NHibernate.Repositories;
+using NHibernate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,6 +43,137 @@ namespace Biz.OdZeraDDD.Tests.RepostitoryTests
       Assert.Equal("PR1", savedProdukt.Symbol);
       Assert.Equal(true, savedProdukt.CzyAktywny);
       Assert.Equal(12.99m, savedProdukt.CenaNetto);
+    }
+
+    [Fact]
+    public void Produkt_Get_Exists()
+    {
+      // Arrange 
+      Produkt produkt = new Produkt
+      {
+        Id = new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"),
+        Nazwa = "Produkt 1",
+        StawkaVAT = 0.23m,
+        Symbol = "PR1",
+        CzyAktywny = true,
+        CenaNetto = 12.99m
+      };
+
+      var produktRepository = new ProduktRepository(Session);
+      produktRepository.Add(produkt);
+      Session.Flush();
+
+      // Act
+      Produkt savedProdukt = produktRepository.Get(new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"));
+
+      // Assert
+      Assert.NotNull(savedProdukt);
+      Assert.Equal(new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"), savedProdukt.Id);
+      Assert.Equal("Produkt 1", savedProdukt.Nazwa);
+      Assert.Equal(0.23m, savedProdukt.StawkaVAT);
+      Assert.Equal("PR1", savedProdukt.Symbol);
+      Assert.Equal(true, savedProdukt.CzyAktywny);
+      Assert.Equal(12.99m, savedProdukt.CenaNetto);
+    }
+
+    [Fact]
+    public void Produkt_Get_NotExists()
+    {
+      // Arrange 
+      var produktRepository = new ProduktRepository(Session);
+
+      // Act
+      Produkt savedProdukt = produktRepository.Get(new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"));
+
+      // Assert
+      Assert.Null(savedProdukt);
+    }
+
+    [Fact]
+    public void Produkt_GetAll_Exists()
+    {
+      // Arrange 
+      var produktRepository = new ProduktRepository(Session);
+
+      Produkt produkt1 = new Produkt
+      {
+        Id = new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"),
+        Nazwa = "Produkt 1",
+        StawkaVAT = 0.23m,
+        Symbol = "PR1",
+        CzyAktywny = true,
+        CenaNetto = 12.99m
+      };
+      Produkt produkt2 = new Produkt
+      {
+        Id = new Guid("7bc79ed5-a97d-4d7e-8d36-a4e8f235b59f"),
+        Nazwa = "Produkt 2",
+        StawkaVAT = 0.08m,
+        Symbol = "PR2",
+        CzyAktywny = true,
+        CenaNetto = 22.79m
+      };
+
+      produktRepository.Add(produkt1);
+      produktRepository.Add(produkt2);
+      Session.Flush();
+
+      // Act
+      IEnumerable<Produkt> savedProduktList = produktRepository.GetAll();
+
+      // Assert
+      Assert.NotNull(savedProduktList);
+      Assert.Equal(2, savedProduktList.Count());
+    }
+
+    [Fact]
+    public void Produkt_GetAll_Empty()
+    {
+      // Arrange 
+      var produktRepository = new ProduktRepository(Session);
+
+      // Act
+      IEnumerable<Produkt> savedProduktList = produktRepository.GetAll();
+
+      // Assert
+      Assert.NotNull(savedProduktList);
+      Assert.Equal(0, savedProduktList.Count());
+    }
+
+    [Fact]
+    public void Produkt_RemoveById_Exists()
+    {
+      // Arrange 
+      Produkt produkt = new Produkt
+      {
+        Id = new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"),
+        Nazwa = "Produkt 1",
+        StawkaVAT = 0.23m,
+        Symbol = "PR1",
+        CzyAktywny = true,
+        CenaNetto = 12.99m
+      };
+
+      var produktRepository = new ProduktRepository(Session);
+      produktRepository.Add(produkt);
+      Session.Flush();
+
+      // Act
+      produktRepository.Delete(new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"));
+
+      // Assert
+      Produkt savedProdukt = produktRepository.Get(new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6"));
+      Assert.Null(savedProdukt);
+    }
+
+    [Fact]
+    public void Produkt_RemoveById_NotExists()
+    {
+      // Arrange 
+      var produktRepository = new ProduktRepository(Session);
+
+      // Act
+      Assert.Throws<ObjectNotFoundException>(() => produktRepository.Delete(new Guid("be7bdc8f-c8fa-473a-975e-848d7600aae6")));
     }
   }
 }
